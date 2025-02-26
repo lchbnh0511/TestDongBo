8908bf984f56d0d08fcd58586b38f6ad6b6ed208
unknown <lechibinh0511@gmail.com>
2025-02-26 17:18:18 +0700

    Khởi tạo dự án Unity

---
+++ Assets/Scripts/Project1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Project1 : MonoBehaviour
+{
+    [SerializeField] private Button buttonTest;   
+    public void OnClick_Test()
+    {
+        Debug.Log("Vao day ne");
+
+    }
+}
